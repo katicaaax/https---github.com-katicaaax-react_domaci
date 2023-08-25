e575f0b35962844b541f23f15d4c9e6497165505
katicaaax <radosavljevic.katarina98@gmail.com>
2023-08-25 23:11:14 +0200

    Handling Events. Prvi commit.

--- src/oneProduct.jsx
+++ src/oneProduct.jsx
@@ -6,6 +6,18 @@ import Products from "./products";
 const OneProduct = ({product}) => {
     //pravimo promenljivu za stilizovanje
     const design = {margin: 10, borderStyle: "dashed"};
+    const addToCart = () => {
+        product.amount = product.amount + 1;
+        console.log("product id=", product.id, "amount=", product.amount);
+    };
+    const remFromCart = () => {
+        if(product.amount > 0){
+            product.amount = product.amount - 1;
+            console.log("product id=", product.id, "amount=", product.amount);
+        }else{
+            alert("Amount of product is already 0.");
+        }
+    };
   return (
     //prosledjujemo promenljivu za stilizovanje
     <div className="card" style = {design}> 
@@ -13,10 +25,10 @@ const OneProduct = ({product}) => {
         <div className="card-body">
             <h3 className="card-title">{product.title}</h3>
             <p className="card-text">{product.description}</p>
-            <button className="btn">
+            <button className="btn" onClick={addToCart}>
                 < ImCheckboxChecked />
             </button>
-            <button className="btn">
+            <button className="btn" onClick={remFromCart}>
                 <FaMinusSquare />
                 </button>
         </div>
