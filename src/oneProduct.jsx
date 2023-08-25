7bbb8093e9bdbf7af811f77b05e50d02ba95508b
katicaaax <radosavljevic.katarina98@gmail.com>
2023-08-25 19:25:25 +0200

    Stilizovanje. Prvi deo.

--- src/oneProduct.jsx
+++ src/oneProduct.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 const OneProduct = () => {
+    //pravimo promenljivu za stilizovanje
+    const design = {margin: 10, borderStyle: "dashed"};
   return (
-    <div className="card">
+    //prosledjujemo promenljivu za stilizovanje
+    <div className="card" style = {design}> 
         <img className="card-img-top" src="https://picsum.photos/200" alt="Neka slika"/>
         <div className="card-body">
             <h3 className="card-title">Product name</h3>
